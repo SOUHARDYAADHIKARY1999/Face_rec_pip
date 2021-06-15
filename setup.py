53f201d1ce9898f8deef554d4176dbb62ca49af9
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2021-06-15 23:39:28 +0530

    changes added

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'SOUHARDYA ADHIKARY',                   # Type in your name
   author_email = 'souhardyaadhikary86942@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/SOUHARDYAADHIKARY1999/Face_rec_pip',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/SOUHARDYAADHIKARY1999/Face_rec_pip/archive/refs/tags/v_1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/SOUHARDYAADHIKARY1999/Face_rec_pip/archive/refs/tags/v_2.0.tar.gz',    # I explain this later on
   keywords = ['FACE', 'RECOGNITION', 'PERSONALITIES'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'keras',

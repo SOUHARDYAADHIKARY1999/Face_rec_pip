d76ecdfb3a0e7be351cac946bf876e158a608220
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2021-06-15 23:18:11 +0530

    install requirements are added

--- setup.py
+++ setup.py
@@ -11,14 +11,10 @@ setup(
   download_url = 'https://github.com/SOUHARDYAADHIKARY1999/Face_rec_pip/archive/refs/tags/v_1.0.tar.gz',    # I explain this later on
   keywords = ['FACE', 'RECOGNITION', 'PERSONALITIES'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-          'kears',
+          'keras',
           'pickle',
           'cv2',
           'numpy',
-          'sys',
-          'os',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package

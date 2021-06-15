895c530d175deb15adfad8db14d51a25ca6afefe
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2021-06-15 23:13:32 +0530

    setup file upgraded

--- setup.py
+++ setup.py
@@ -13,6 +13,12 @@ setup(
   install_requires=[            # I get to this in a second
           'validators',
           'beautifulsoup4',
+          'kears',
+          'pickle',
+          'cv2',
+          'numpy',
+          'sys',
+          'os',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
